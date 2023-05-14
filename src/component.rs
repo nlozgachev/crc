d1af4158005dfef28a763955b46fb15f122d2c4a
Nikita <lozgachev@posteo.net>
2023-05-14 14:23:59 +0400

    chore: static typing for templates

--- src/component.rs
+++ src/component.rs
@@ -12,7 +12,7 @@ pub fn create_component_hashmap<T>(index: T, view: T, styles: T) -> HashMap<&'st
 }
 
 pub fn setup_component_contents(
-    templates: HashMap<&'static str, String>,
+    templates: HashMap<&'static str, &'static str>,
     component_name: &str,
 ) -> HashMap<&'static str, String> {
     let mut hmap: HashMap<&'static str, String> = HashMap::new();
@@ -28,7 +28,7 @@ pub fn create_component(path_string: &str, component_name: &str) {
     create_component_dir(path_string, component_name);
 
     let out_dir: String = get_component_dir(path_string, component_name);
-    let templates: HashMap<&'static str, String> = embed_templates();
+    let templates: HashMap<&'static str, &'static str> = embed_templates();
     let component_contents: HashMap<&'static str, String> =
         setup_component_contents(templates, component_name);
     let file_paths = get_new_file_paths(&out_dir, component_name, &component_contents);
--- src/templates.rs
+++ src/templates.rs
@@ -2,10 +2,10 @@ use std::collections::HashMap;
 
 use crate::component::create_component_hashmap;
 
-pub fn embed_templates() -> HashMap<&'static str, String> {
+pub fn embed_templates() -> HashMap<&'static str, &'static str> {
     return create_component_hashmap(
-        include_str!("templates/index.ts").to_string(),
-        include_str!("templates/COMPONENT.view.tsx").to_string(),
-        include_str!("templates/COMPONENT.styles.ts").to_string(),
+        include_str!("templates/index.ts"),
+        include_str!("templates/COMPONENT.view.tsx"),
+        include_str!("templates/COMPONENT.styles.ts"),
     );
 }
